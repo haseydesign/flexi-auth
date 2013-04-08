2bebffdb560720a5fae1f21e755d75d372fd5013
Rob Hussey <rob@haseydesign.com>
2013-04-08 21:58:19 +1000

    Updated the library and demo database dumps to include default values

--- demo_files/sql_dump/flexi_auth_demo_database_dump.sql
+++ demo_files/sql_dump/flexi_auth_demo_database_dump.sql
@@ -1,16 +1,5 @@
 /*
-Navicat MySQL Data Transfer
-
-Source Server         :  Localhost
-Source Server Version : 50141
-Source Host           : localhost:3306
-Source Database       : flexi_auth_demo
-
-Target Server Type    : MYSQL
-Target Server Version : 50141
-File Encoding         : 65001
-
-Date: 2012-04-12 22:40:38
+  flexi_auth_demo_database_dump.sql
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -23,7 +12,7 @@ CREATE TABLE `ci_sessions` (
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) DEFAULT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
+  `user_data` text NOT NULL DEFAULT '',
   PRIMARY KEY (`session_id`),
   KEY `last_activity` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -38,17 +27,17 @@ CREATE TABLE `ci_sessions` (
 DROP TABLE IF EXISTS `demo_user_address`;
 CREATE TABLE `demo_user_address` (
   `uadd_id` int(11) NOT NULL AUTO_INCREMENT,
-  `uadd_uacc_fk` int(11) NOT NULL,
-  `uadd_alias` varchar(50) NOT NULL,
-  `uadd_recipient` varchar(100) NOT NULL,
-  `uadd_phone` varchar(25) NOT NULL,
-  `uadd_company` varchar(75) NOT NULL,
-  `uadd_address_01` varchar(100) NOT NULL,
-  `uadd_address_02` varchar(100) NOT NULL,
-  `uadd_city` varchar(50) NOT NULL,
-  `uadd_county` varchar(50) NOT NULL,
-  `uadd_post_code` varchar(25) NOT NULL,
-  `uadd_country` varchar(50) NOT NULL,
+  `uadd_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `uadd_alias` varchar(50) NOT NULL DEFAULT '',
+  `uadd_recipient` varchar(100) NOT NULL DEFAULT '',
+  `uadd_phone` varchar(25) NOT NULL DEFAULT '',
+  `uadd_company` varchar(75) NOT NULL DEFAULT '',
+  `uadd_address_01` varchar(100) NOT NULL DEFAULT '',
+  `uadd_address_02` varchar(100) NOT NULL DEFAULT '',
+  `uadd_city` varchar(50) NOT NULL DEFAULT '',
+  `uadd_county` varchar(50) NOT NULL DEFAULT '',
+  `uadd_post_code` varchar(25) NOT NULL DEFAULT '',
+  `uadd_country` varchar(50) NOT NULL DEFAULT '',
   PRIMARY KEY (`uadd_id`),
   UNIQUE KEY `uadd_id` (`uadd_id`),
   KEY `uadd_uacc_fk` (`uadd_uacc_fk`)
@@ -66,11 +55,11 @@ INSERT INTO `demo_user_address` VALUES ('2', '4', 'Work', 'Joe Public', '0123456
 DROP TABLE IF EXISTS `demo_user_profiles`;
 CREATE TABLE `demo_user_profiles` (
   `upro_id` int(11) NOT NULL AUTO_INCREMENT,
-  `upro_uacc_fk` int(11) NOT NULL,
-  `upro_company` varchar(50) NOT NULL,
-  `upro_first_name` varchar(50) NOT NULL,
-  `upro_last_name` varchar(50) NOT NULL,
-  `upro_phone` varchar(25) NOT NULL,
+  `upro_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `upro_company` varchar(50) NOT NULL DEFAULT '',
+  `upro_first_name` varchar(50) NOT NULL DEFAULT '',
+  `upro_last_name` varchar(50) NOT NULL DEFAULT '',
+  `upro_phone` varchar(25) NOT NULL DEFAULT '',
   `upro_newsletter` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`upro_id`),
   UNIQUE KEY `upro_id` (`upro_id`),
@@ -90,24 +79,24 @@ INSERT INTO `demo_user_profiles` VALUES ('3', '3', '', 'Joe', 'Public', '0123456
 DROP TABLE IF EXISTS `user_accounts`;
 CREATE TABLE `user_accounts` (
   `uacc_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `uacc_group_fk` smallint(5) unsigned NOT NULL,
-  `uacc_email` varchar(100) NOT NULL,
-  `uacc_username` varchar(15) NOT NULL,
-  `uacc_password` varchar(60) NOT NULL,
-  `uacc_ip_address` varchar(40) NOT NULL,
-  `uacc_salt` varchar(40) NOT NULL,
-  `uacc_activation_token` varchar(40) NOT NULL,
-  `uacc_forgotten_password_token` varchar(40) NOT NULL,
-  `uacc_forgotten_password_expire` datetime NOT NULL,
-  `uacc_update_email_token` varchar(40) NOT NULL,
-  `uacc_update_email` varchar(100) NOT NULL,
-  `uacc_active` tinyint(1) unsigned NOT NULL,
-  `uacc_suspend` tinyint(1) unsigned NOT NULL,
-  `uacc_fail_login_attempts` smallint(5) NOT NULL,
-  `uacc_fail_login_ip_address` varchar(40) NOT NULL,
-  `uacc_date_fail_login_ban` datetime NOT NULL COMMENT 'Time user is banned until due to repeated failed logins',
-  `uacc_date_last_login` datetime NOT NULL,
-  `uacc_date_added` datetime NOT NULL,
+  `uacc_group_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `uacc_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_username` varchar(15) NOT NULL DEFAULT '',
+  `uacc_password` varchar(60) NOT NULL DEFAULT '',
+  `uacc_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_salt` varchar(40) NOT NULL DEFAULT '',
+  `uacc_activation_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_expire` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_update_email_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_update_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_suspend` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_fail_login_attempts` smallint(5) NOT NULL DEFAULT '0',
+  `uacc_fail_login_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_date_fail_login_ban` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time user is banned until due to repeated failed logins',
+  `uacc_date_last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`uacc_id`),
   UNIQUE KEY `uacc_id` (`uacc_id`),
   KEY `uacc_group_fk` (`uacc_group_fk`),
@@ -129,9 +118,9 @@ INSERT INTO `user_accounts` VALUES ('3', '1', 'public@public.com', 'public', '$2
 DROP TABLE IF EXISTS `user_groups`;
 CREATE TABLE `user_groups` (
   `ugrp_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `ugrp_name` varchar(20) NOT NULL,
-  `ugrp_desc` varchar(100) NOT NULL,
-  `ugrp_admin` tinyint(1) NOT NULL,
+  `ugrp_name` varchar(20) NOT NULL DEFAULT '',
+  `ugrp_desc` varchar(100) NOT NULL DEFAULT '',
+  `ugrp_admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ugrp_id`),
   UNIQUE KEY `ugrp_id` (`ugrp_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
@@ -148,10 +137,10 @@ INSERT INTO `user_groups` VALUES ('3', 'Master Admin', 'Master Admin : has full 
 -- ----------------------------
 DROP TABLE IF EXISTS `user_login_sessions`;
 CREATE TABLE `user_login_sessions` (
-  `usess_uacc_fk` int(11) NOT NULL,
-  `usess_series` varchar(40) NOT NULL,
-  `usess_token` varchar(40) NOT NULL,
-  `usess_login_date` datetime NOT NULL,
+  `usess_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `usess_series` varchar(40) NOT NULL DEFAULT '',
+  `usess_token` varchar(40) NOT NULL DEFAULT '',
+  `usess_login_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`usess_token`),
   UNIQUE KEY `usess_token` (`usess_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -166,8 +155,8 @@ CREATE TABLE `user_login_sessions` (
 DROP TABLE IF EXISTS `user_privileges`;
 CREATE TABLE `user_privileges` (
   `upriv_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `upriv_name` varchar(20) NOT NULL,
-  `upriv_desc` varchar(100) NOT NULL,
+  `upriv_name` varchar(20) NOT NULL DEFAULT '',
+  `upriv_desc` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`upriv_id`),
   UNIQUE KEY `upriv_id` (`upriv_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
@@ -193,8 +182,8 @@ INSERT INTO `user_privileges` VALUES ('11', 'Delete Privileges', 'User can delet
 DROP TABLE IF EXISTS `user_privilege_users`;
 CREATE TABLE `user_privilege_users` (
   `upriv_users_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `upriv_users_uacc_fk` int(11) NOT NULL,
-  `upriv_users_upriv_fk` smallint(5) NOT NULL,
+  `upriv_users_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `upriv_users_upriv_fk` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`upriv_users_id`),
   UNIQUE KEY `upriv_users_id` (`upriv_users_id`) USING BTREE,
   KEY `upriv_users_uacc_fk` (`upriv_users_uacc_fk`),
@@ -228,8 +217,8 @@ INSERT INTO `user_privilege_users` VALUES ('15', '2', '6');
 DROP TABLE IF EXISTS `user_privilege_groups`;
 CREATE TABLE `user_privilege_groups` (
   `upriv_groups_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL,
-  `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL,
+  `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`upriv_groups_id`),
   UNIQUE KEY `upriv_groups_id` (`upriv_groups_id`) USING BTREE,
   KEY `upriv_groups_ugrp_fk` (`upriv_groups_ugrp_fk`),
--- library_files/sql_dump/flexi_auth_database_dump.sql
+++ library_files/sql_dump/flexi_auth_database_dump.sql
@@ -1,16 +1,5 @@
 /*
-Navicat MySQL Data Transfer
-
-Source Server         :  Localhost
-Source Server Version : 50524
-Source Host           : localhost:3306
-Source Database       : flexi_auth
-
-Target Server Type    : MYSQL
-Target Server Version : 50524
-File Encoding         : 65001
-
-Date: 2012-09-15 15:02:25
+  flexi_auth_database_dump.sql
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -24,7 +13,7 @@ CREATE TABLE `ci_sessions` (
   `ip_address` varchar(16) NOT NULL DEFAULT '0',
   `user_agent` varchar(120) DEFAULT NULL,
   `last_activity` int(10) unsigned NOT NULL DEFAULT '0',
-  `user_data` text NOT NULL,
+  `user_data` text NOT NULL DEFAULT '',
   PRIMARY KEY (`session_id`),
   KEY `last_activity` (`last_activity`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -39,24 +28,24 @@ CREATE TABLE `ci_sessions` (
 DROP TABLE IF EXISTS `user_accounts`;
 CREATE TABLE `user_accounts` (
   `uacc_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `uacc_group_fk` smallint(5) unsigned NOT NULL,
-  `uacc_email` varchar(100) NOT NULL,
-  `uacc_username` varchar(15) NOT NULL,
-  `uacc_password` varchar(60) NOT NULL,
-  `uacc_ip_address` varchar(40) NOT NULL,
-  `uacc_salt` varchar(40) NOT NULL,
-  `uacc_activation_token` varchar(40) NOT NULL,
-  `uacc_forgotten_password_token` varchar(40) NOT NULL,
-  `uacc_forgotten_password_expire` datetime NOT NULL,
-  `uacc_update_email_token` varchar(40) NOT NULL,
-  `uacc_update_email` varchar(100) NOT NULL,
-  `uacc_active` tinyint(1) unsigned NOT NULL,
-  `uacc_suspend` tinyint(1) unsigned NOT NULL,
-  `uacc_fail_login_attempts` smallint(5) NOT NULL,
-  `uacc_fail_login_ip_address` varchar(40) NOT NULL,
-  `uacc_date_fail_login_ban` datetime NOT NULL COMMENT 'Time user is banned until due to repeated failed logins',
-  `uacc_date_last_login` datetime NOT NULL,
-  `uacc_date_added` datetime NOT NULL,
+  `uacc_group_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `uacc_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_username` varchar(15) NOT NULL DEFAULT '',
+  `uacc_password` varchar(60) NOT NULL DEFAULT '',
+  `uacc_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_salt` varchar(40) NOT NULL DEFAULT '',
+  `uacc_activation_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_forgotten_password_expire` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_update_email_token` varchar(40) NOT NULL DEFAULT '',
+  `uacc_update_email` varchar(100) NOT NULL DEFAULT '',
+  `uacc_active` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_suspend` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `uacc_fail_login_attempts` smallint(5) NOT NULL DEFAULT '0',
+  `uacc_fail_login_ip_address` varchar(40) NOT NULL DEFAULT '',
+  `uacc_date_fail_login_ban` datetime NOT NULL DEFAULT '0000-00-00 00:00:00' COMMENT 'Time user is banned until due to repeated failed logins',
+  `uacc_date_last_login` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `uacc_date_added` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`uacc_id`),
   UNIQUE KEY `uacc_id` (`uacc_id`),
   KEY `uacc_group_fk` (`uacc_group_fk`),
@@ -75,9 +64,9 @@ CREATE TABLE `user_accounts` (
 DROP TABLE IF EXISTS `user_groups`;
 CREATE TABLE `user_groups` (
   `ugrp_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `ugrp_name` varchar(20) NOT NULL,
-  `ugrp_desc` varchar(100) NOT NULL,
-  `ugrp_admin` tinyint(1) NOT NULL,
+  `ugrp_name` varchar(20) NOT NULL DEFAULT '',
+  `ugrp_desc` varchar(100) NOT NULL DEFAULT '',
+  `ugrp_admin` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`ugrp_id`),
   UNIQUE KEY `ugrp_id` (`ugrp_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
@@ -91,10 +80,10 @@ CREATE TABLE `user_groups` (
 -- ----------------------------
 DROP TABLE IF EXISTS `user_login_sessions`;
 CREATE TABLE `user_login_sessions` (
-  `usess_uacc_fk` int(11) NOT NULL,
-  `usess_series` varchar(40) NOT NULL,
-  `usess_token` varchar(40) NOT NULL,
-  `usess_login_date` datetime NOT NULL,
+  `usess_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `usess_series` varchar(40) NOT NULL DEFAULT '',
+  `usess_token` varchar(40) NOT NULL DEFAULT '',
+  `usess_login_date` datetime NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`usess_token`),
   UNIQUE KEY `usess_token` (`usess_token`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
@@ -109,8 +98,8 @@ CREATE TABLE `user_login_sessions` (
 DROP TABLE IF EXISTS `user_privileges`;
 CREATE TABLE `user_privileges` (
   `upriv_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `upriv_name` varchar(20) NOT NULL,
-  `upriv_desc` varchar(100) NOT NULL,
+  `upriv_name` varchar(20) NOT NULL DEFAULT '',
+  `upriv_desc` varchar(100) NOT NULL DEFAULT '',
   PRIMARY KEY (`upriv_id`),
   UNIQUE KEY `upriv_id` (`upriv_id`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
@@ -125,8 +114,8 @@ CREATE TABLE `user_privileges` (
 DROP TABLE IF EXISTS `user_privilege_users`;
 CREATE TABLE `user_privilege_users` (
   `upriv_users_id` smallint(5) NOT NULL AUTO_INCREMENT,
-  `upriv_users_uacc_fk` int(11) NOT NULL,
-  `upriv_users_upriv_fk` smallint(5) NOT NULL,
+  `upriv_users_uacc_fk` int(11) NOT NULL DEFAULT '0',
+  `upriv_users_upriv_fk` smallint(5) NOT NULL DEFAULT '0',
   PRIMARY KEY (`upriv_users_id`),
   UNIQUE KEY `upriv_users_id` (`upriv_users_id`) USING BTREE,
   KEY `upriv_users_uacc_fk` (`upriv_users_uacc_fk`),
@@ -145,8 +134,8 @@ CREATE TABLE `user_privilege_users` (
 DROP TABLE IF EXISTS `user_privilege_groups`;
 CREATE TABLE `user_privilege_groups` (
   `upriv_groups_id` smallint(5) unsigned NOT NULL AUTO_INCREMENT,
-  `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL,
-  `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL,
+  `upriv_groups_ugrp_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `upriv_groups_upriv_fk` smallint(5) unsigned NOT NULL DEFAULT '0',
   PRIMARY KEY (`upriv_groups_id`),
   UNIQUE KEY `upriv_groups_id` (`upriv_groups_id`) USING BTREE,
   KEY `upriv_groups_ugrp_fk` (`upriv_groups_ugrp_fk`),
